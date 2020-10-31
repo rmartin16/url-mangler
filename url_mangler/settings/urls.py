4cee590e72741d59f81de3cc709bd690bd3fe2ac
Russell Martin <rmartin16@gmail.com>
2020-10-31 15:13:13 -0400

    start urls

--- url_mangler/settings/urls.py
+++ url_mangler/settings/urls.py
@@ -16,4 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [path("admin/", admin.site.urls), path("r/")]
+# fmt: off
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # path("r/")
+]
